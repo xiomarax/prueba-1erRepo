5d70773e65131f59b3962493e955be44ec4f30c7
xiomarax <133995088+xiomarax@users.noreply.github.com>
2023-10-01 15:00:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ function Hola(camada) {
     console.log("Hola"+ camada)
 }
 
-function Chao(camda) {
+function ChaoChao(camda) {
     console.log("Chao" +camada)
 }
 
 Hola("Camada1")
-Chao("Camada1")
+ChaoChao("Camada1")
